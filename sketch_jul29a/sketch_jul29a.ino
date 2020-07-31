e601c58045f799fa06af27c7dffb0939e75b4397
bao007fei <bao007fei@gmail.com>
2020-08-01 03:27:46 +0800

    Update sketch_jul29a.ino

--- sketch_jul29a/sketch_jul29a.ino
+++ sketch_jul29a/sketch_jul29a.ino
@@ -1,46 +1,23 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 int bitPins[4] = {0, 1, 2, 3};
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 int addPin = 6;
 int clearPin = 7;
 
 int counter = 0;
-=======
->>>>>>> parent of f6c821f... use varibles
-=======
->>>>>>> parent of f6c821f... use varibles
-=======
-int bitPins[4] = {4, 5, 6, 7};
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-int bitPins[4] = {4, 5, 6, 7};
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-int bitPins[4] = {4, 5, 6, 7};
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-int bitPins[4] = {4, 5, 6, 7};
->>>>>>> parent of 63c9cca... almost a binary counter
 
 void setup()
 {
   for (size_t i = 0; i < 4; i++)
   {
-    pinMode(i, OUTPUT);
+    pinMode(bitPins[i], OUTPUT);
   }
+
+  pinMode(addPin, INPUT);
+  pinMode(clearPin, INPUT);
 }
 
 void loop()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   if (digitalRead(addPin) == LOW)
   {
     counter++;
@@ -62,98 +39,11 @@ void binaryDisplay()
   {
     if (bitRead(counter, j) == 1)
     {
-<<<<<<< HEAD
       digitalWrite(bitPins[j], HIGH);
     }
     else
     {
       digitalWrite(bitPins[j], LOW);
-=======
-      if (bitRead(i, j) == 1)
-      {
-        digitalWrite(j, HIGH);
-      }
-      else
-      {
-        digitalWrite(j, LOW);
-      }
->>>>>>> parent of f6c821f... use varibles
     }
-=======
-  for (size_t i = 0; i < 16; i++)
-  {
-    Serial.println(i);
-
-    for (size_t j = 0; j < 4; j++)
-    {
-      if (bitRead(i, j) == 1)
-      {
-        digitalWrite(bitPins[j], HIGH);
-      }
-      else
-      {
-        digitalWrite(bitPins[j], LOW);
-      }
-    }
-
-    delay(1000);
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-  for (size_t i = 0; i < 16; i++)
-  {
-    Serial.println(i);
-
-    for (size_t j = 0; j < 4; j++)
-    {
-      if (bitRead(i, j) == 1)
-      {
-        digitalWrite(bitPins[j], HIGH);
-      }
-      else
-      {
-        digitalWrite(bitPins[j], LOW);
-      }
-    }
-
-    delay(1000);
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-  for (size_t i = 0; i < 16; i++)
-  {
-    Serial.println(i);
-
-    for (size_t j = 0; j < 4; j++)
-    {
-      if (bitRead(i, j) == 1)
-      {
-        digitalWrite(bitPins[j], HIGH);
-      }
-      else
-      {
-        digitalWrite(bitPins[j], LOW);
-      }
-    }
-
-    delay(1000);
->>>>>>> parent of 63c9cca... almost a binary counter
-=======
-  for (size_t i = 0; i < 16; i++)
-  {
-    Serial.println(i);
-
-    for (size_t j = 0; j < 4; j++)
-    {
-      if (bitRead(i, j) == 1)
-      {
-        digitalWrite(bitPins[j], HIGH);
-      }
-      else
-      {
-        digitalWrite(bitPins[j], LOW);
-      }
-    }
-
-    delay(1000);
->>>>>>> parent of 63c9cca... almost a binary counter
   }
 }
