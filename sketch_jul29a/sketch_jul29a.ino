48cd635e32a4a18c63a97650f056b50998c8dd29
bao007fei <bao007fei@gmail.com>
2020-08-01 03:03:02 +0800

    Update sketch_jul29a.ino

--- sketch_jul29a/sketch_jul29a.ino
+++ sketch_jul29a/sketch_jul29a.ino
@@ -1,15 +1,18 @@
 int bitPins[4] = {0, 1, 2, 3};
+<<<<<<< HEAD
 
 int addPin = 6;
 int clearPin = 7;
 
 int counter = 0;
+=======
+>>>>>>> parent of f6c821f... use varibles
 
 void setup()
 {
   for (size_t i = 0; i < 4; i++)
   {
-    pinMode(bitPins[i], OUTPUT);
+    pinMode(i, OUTPUT);
   }
 
   pinMode(addPin, INPUT);
@@ -39,11 +42,22 @@ void binaryDisplay()
   {
     if (bitRead(counter, j) == 1)
     {
+<<<<<<< HEAD
       digitalWrite(bitPins[j], HIGH);
     }
     else
     {
       digitalWrite(bitPins[j], LOW);
+=======
+      if (bitRead(i, j) == 1)
+      {
+        digitalWrite(j, HIGH);
+      }
+      else
+      {
+        digitalWrite(j, LOW);
+      }
+>>>>>>> parent of f6c821f... use varibles
     }
   }
 }
