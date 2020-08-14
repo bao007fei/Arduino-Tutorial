97f2f06432ee3f78b80debadfdc1ab7eb2de70f5
bao007fei <bao007fei@gmail.com>
2020-08-15 01:11:19 +0800

    Update TFT.ino

--- TFT/TFT.ino
+++ TFT/TFT.ino
@@ -36,6 +36,8 @@ void setup(void)
 
   tft.fillScreen(ST7735_BLACK);
   tft.setTextWrap(false);
+  tft.setTextColor(ST7735_GREEN);
+
   delay(500);
 }
 
@@ -45,28 +47,30 @@ void loop()
   int new_t = DHT11.temperature;
   int new_h = DHT11.humidity;
 
-  if (old_t != new_t || old_h != new_h)
+  if (old_t != new_t)
   {
-    old_t = new_t;
-    old_h = new_h;
-
     tft.setCursor(0, 0);
-    tft.setTextColor(ST7735_GREEN);
     tft.setTextSize(1);
     tft.println("Temperature:\n");
     tft.setTextSize(3);
     tft.fillRect(0, 10, 40, 30, ST7735_BLACK);
-    tft.print(old_t);
+    tft.print(new_t);
     tft.print(" C");
 
+    old_t = new_t;
+  }
 
+  if (old_h != new_h)
+  {
     tft.setCursor(0, 70);
     tft.setTextSize(1);
     tft.println("Humidity:\n");
     tft.setTextSize(3);
     tft.fillRect(0, 80, 40, 30, ST7735_BLACK);
-    tft.print(old_h);
+    tft.print(new_h);
     tft.print(" %");
+
+    old_h = new_h;
   }
 
   delay(1000);
