6ad36c866a59d0a87b11ccf16f8102291d740435
bao007fei <bao007fei@gmail.com>
2020-08-26 03:04:33 +0800

    Create test.ino

---
+++ test/test.ino
@@ -0,0 +1,23 @@
+int redLED = 9;
+int bright = 1;
+
+void setup()
+{
+    pinMode(redLED, OUTPUT);
+}
+
+void loop()
+{
+
+    for (size_t i = 0; i < 255; i++)
+    {
+        analogWrite(redLED, i);
+        delay(10);
+    }
+
+    for (size_t i = 255; i > 0; i--)
+    {
+        analogWrite(redLED, i);
+        delay(10);
+    }
+}
